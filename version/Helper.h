4321154851ccce93c2b070f7fd7997e9bc7443c6
MQsecx2 <116274995+MQsecx2@users.noreply.github.com>
2023-01-19 23:23:57 +0400

    Create Helper.h

---
+++ version/Helper.h
@@ -0,0 +1,16 @@
+#ifndef _HELPER_
+#define _HELPER_
+
+#pragma comment(lib, "dbghelp.lib") 
+
+#include <stdio.h>
+#include <windows.h>
+#include <DbgHelp.h>
+#include <WinTrust.h>
+#include <time.h>
+
+extern void decrypt(unsigned char* data, long dataLen, unsigned char* key, long keyLen, unsigned char* result);
+extern BOOL IsWow64(HANDLE pHandle);
+extern DWORD FindPID(const char* procname);
+
+#endif
